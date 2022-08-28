f82877369f71fab1ba608b37812f57bed3300964
dasiux <me@siux.info>
2022-08-28 03:50:20 +0200

    Create index.js

---
+++ src/es6/index.js
@@ -0,0 +1,19 @@
+/* !
+ * @module      : @squirrel-forge/ui-video
+ * @version     : 0.5.0
+ * @license     : MIT
+ * @copyright   : 2022 squirrel-forge
+ * @author      : Daniel Hartwell aka. siux <me@siux.info>
+ * @description : An accessible video player with events and plugin support, made for the browser and babel compatible.
+ */
+
+/**
+ * Video
+ */
+export { UiVideoComponent } from './Video/UiVideoComponent.js';
+
+/**
+ * Plugins
+ */
+export { UiVideoPluginResponsive } from './Plugins/UiVideoPluginResponsive';
+export { UiVideoPluginTracking } from './Plugins/UiVideoPluginTracking';
