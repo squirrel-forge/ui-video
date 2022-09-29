175aa2aca32f974f4bf766d89a333e8ac18033ca
dasiux <me@siux.info>
2022-09-29 17:44:50 +0200

    cleanup + label display + code comments

--- src/es6/Plugins/UiVideoPluginSound.js
+++ src/es6/Plugins/UiVideoPluginSound.js
@@ -39,6 +39,11 @@ export class UiVideoPluginSound extends UiPlugin {
      */
     #last_volume = 0;
 
+    /**
+     * Marker for first unmute action
+     * @private
+     * @type {boolean}
+     */
     #first_unmute = true;
 
     /**
@@ -88,6 +93,10 @@ export class UiVideoPluginSound extends UiPlugin {
                 // @type {number}
                 firstunmute : 65,
 
+                // Add css custom properties with volume number and percentage
+                // @type {boolean}
+                cssprop : true,
+
                 // Percent decimals
                 // @type {Object}
                 decimals : {
@@ -109,12 +118,30 @@ export class UiVideoPluginSound extends UiPlugin {
                 // Sound button references
                 // @type {object}
                 sound : {
+
+                    // Sound control wrapper
+                    // @type {string}
                     control : '.ui-video__control--sound',
+
+                    // Mute toggle button
+                    // @type {string}
                     on : '[data-video="ctrl:mute"]',
+
+                    // Unmute toggle button
+                    // @type {string}
                     off : '[data-video="ctrl:unmute"]',
+
+                    // Volume input range control
+                    // @type {string}
                     volume : '[data-video="ctrl:volume"]',
-                    trigger : '[data-video="ctrl:volumetrig"]',
+
+                    // Icon no sound available
+                    // @type {string}
                     none : '[data-video="ctrl:nosound"]',
+
+                    // Label percent display
+                    // @type {string}
+                    label : '[data-video="label:volume"]',
                 },
             },
         };
@@ -163,13 +190,13 @@ export class UiVideoPluginSound extends UiPlugin {
         const mute = this.context.getDomRefs( 'sound.on', false );
         const unmute = this.context.getDomRefs( 'sound.off', false );
         const volume = this.context.getDomRefs( 'sound.volume', false );
-        // const volumetrig = this.context.getDomRefs( 'sound.volumetrig', false );
+        const label = this.context.getDomRefs( 'sound.label', false );
 
         // Detect audio state
         this.#bind_audiostate();
 
         // Detect volume change
-        this.#bind_volume_change( volume );
+        this.#bind_volume_change( mute, unmute, volume, label );
 
         // Has no controls
         if ( !control ) {
@@ -177,28 +204,35 @@ export class UiVideoPluginSound extends UiPlugin {
             return;
         }
 
+        /**
+         * Set sound available state
+         * @private
+         * @return {void}
+         */
         const sound_available = () => {
             const display = this.context.config.get( 'sound.display' );
-            this.context.constructor.showControl( mute, display );
-            this.context.constructor.showControl( unmute, display );
-            this.context.constructor.showControl( volume, display );
-
-            // TODO: volume trigger only required when no mute/unmute controls are available
-            // this.context.constructor.showControl( volumetrig, display );
-            this.context.constructor.hideControl( none, display );
+            if ( mute ) this.context.constructor.showControl( mute, display );
+            if ( unmute ) this.context.constructor.showControl( unmute, display );
+            if ( volume ) this.context.constructor.showControl( volume, display );
+            if ( none ) this.context.constructor.hideControl( none, display );
             this.#match_current_state( mute, unmute );
         };
+
+        // Treat unknown sound availability as if sound is available
         this.context.addEventListener( 'video.sound.unknown', sound_available );
         this.context.addEventListener( 'video.sound.available', sound_available );
+
+        /**
+         * Set no sound available state
+         * @private
+         * @return {void}
+         */
         this.context.addEventListener( 'video.sound.none', () => {
             const display = this.context.config.get( 'sound.display' );
-            this.context.constructor.hideControl( mute, display );
-            this.context.constructor.hideControl( unmute, display );
-            this.context.constructor.hideControl( volume, display );
-
-            // TODO: volume trigger only required when no mute/unmute controls are available
-            // this.context.constructor.hideControl( volumetrig, display );
-            this.context.constructor.showControl( none, display );
+            if ( mute ) this.context.constructor.hideControl( mute, display );
+            if ( unmute ) this.context.constructor.hideControl( unmute, display );
+            if ( volume ) this.context.constructor.hideControl( volume, display );
+            if ( none ) this.context.constructor.showControl( none, display );
         } );
 
         // Bind mute/unmute toggle
@@ -263,11 +297,13 @@ export class UiVideoPluginSound extends UiPlugin {
      * @param {HTMLButtonElement} mute - Mute button
      * @param {HTMLButtonElement} unmute - Unmute button
      * @param {HTMLInputElement} volume - Input range control
+     * @param {HTMLElement} label - Volume label
      * @return {void}
      */
-    #bind_volume_change( mute, unmute, volume ) {
+    #bind_volume_change( mute, unmute, volume, label ) {
         this.context.video.addEventListener( 'volumechange', () => {
-            this.#set_cssprop( this.context.video.volume * 100 );
+            if ( this.context.config.get( 'sound.cssprop' ) ) this.#set_cssprop( this.context.video.volume * 100 );
+            if ( label ) this.#set_label( label, this.context.video.volume * 100 );
             if ( volume ) volume.value = this.context.video.volume * 100;
             const below_muted_max = this.context.video.volume * 100 <= this.context.config.get( 'sound.mutedmax' );
             if ( below_muted_max ) {
@@ -293,12 +329,31 @@ export class UiVideoPluginSound extends UiPlugin {
         this.context.dom.style.setProperty( '--ui-video-volume-number', percent );
     }
 
-    #match_current_state( mute, unmute, volume ) {
+    /**
+     * Set label text
+     * @param {HTMLElement} label - Label element
+     * @param {number} percent - Percent volume
+     * @return {void}
+     */
+    #set_label( label, percent ) {
+        const decimals = this.context.config.get( 'sound.decimals.label' );
+        if ( decimals !== null ) percent = round( percent, decimals )
+        label.innerText = percent;
+    }
+
+    /**
+     * Match controls to current state
+     * @private
+     * @param {HTMLButtonElement} mute - Mute button
+     * @param {HTMLButtonElement} unmute - Unmute button
+     * @return {void}
+     */
+    #match_current_state( mute, unmute ) {
         const below_muted_max = this.context.video.volume * 100 <= this.context.config.get( 'sound.mutedmax' );
         if ( below_muted_max || this.context.video.muted ) {
-            this.#state_mute( mute, unmute, volume );
+            this.#state_mute( mute, unmute );
         } else {
-            this.#state_unmute( mute, unmute, volume );
+            this.#state_unmute( mute, unmute );
         }
     }
 
@@ -332,6 +387,7 @@ export class UiVideoPluginSound extends UiPlugin {
 
     /**
      * Bind volume slider
+     * @private
      * @param {HTMLInputElement} volume - Input range control
      * @return {void}
      */
@@ -358,16 +414,6 @@ export class UiVideoPluginSound extends UiPlugin {
             this.context.video.volume = parseFloat( volume.value ) / 100;
         });
 
-        /**
-         * Prevent click events from bubbling to anywhere else
-         *  to avoid conflicts with other underlying events
-         * @private
-         * @return {void}
-         */
-        volume.addEventListener( 'click', ( event ) => {
-            event.stopPropagation();
-        });
-
         // Set initial volume
         volume.value = this.context.video.muted ? 0 : this.context.video.volume * 100;
 
@@ -376,7 +422,7 @@ export class UiVideoPluginSound extends UiPlugin {
     }
 
     /**
-     * Mute sound state
+     * set muted state
      * @private
      * @param {HTMLButtonElement} mute - Mute button
      * @param {HTMLButtonElement} unmute - Unmute button
@@ -391,6 +437,13 @@ export class UiVideoPluginSound extends UiPlugin {
         }
     }
 
+    /**
+     * Set unmuted state
+     * @private
+     * @param {HTMLButtonElement} mute - Mute button
+     * @param {HTMLButtonElement} unmute - Unmute button
+     * @return {void}
+     */
     #state_unmute( mute, unmute ) {
         this.context.states.unset( 'muted' );
         if ( mute && unmute ) {
@@ -400,37 +453,94 @@ export class UiVideoPluginSound extends UiPlugin {
         }
     }
 
+    /**
+     * Get is muted state
+     * @public
+     * @return {boolean} - True if muted
+     */
     isMuted() {
         const below_muted_max = this.context.video.volume * 100 <= this.context.config.get( 'sound.mutedmax' );
         return below_muted_max || this.context.video.muted;
     }
 
+    /**
+     * Mute sound
+     * @public
+     * @param {boolean} saveLast - Update last volume saved
+     * @return {void}
+     */
     mute( saveLast = true ) {
         this.#sound_mute( saveLast );
     }
 
+    /**
+     * Unmute sound
+     * @public
+     * @return {void}
+     */
     unmute() {
         this.#sound_unmute();
     }
 
-    volume( volume ) {
+    /**
+     * Get/set volume
+     * @public
+     * @param {null|number} volume - Volume from 0-100
+     * @return {number|void} - Returns current volume without an argument
+     */
+    volume( volume = null ) {
+
+        // Return current volume if no argument is set
+        if ( volume === null ) return this.context.video.volume * 100;
+
+        // Require valid volume value
+        if ( typeof volume !== 'number' || Number.isNaN( volume ) || volume < 0 || volume > 100 ) {
+            throw new UiVideoPluginSoundException( 'Invalid volume value, must be a number from 0 to 100' );
+        }
+
+        // Set new volume
         this.context.video.volume = volume / 100;
     }
 
+    /**
+     * Mute sound and remember volume
+     * @private
+     * @param {boolean} saveLast - Update last volume saved
+     * @return {void}
+     */
     #sound_mute( saveLast ) {
+
+        // Update the last volume saved
         if ( saveLast ) this.#last_volume = this.context.video.volume * 100;
+
+        // Kill volume and set to muted
         this.context.video.volume = 0;
         this.context.video.muted = true;
     }
 
+    /**
+     * Unmute sound and set first/last volume
+     * @private
+     * @return {void}
+     */
     #sound_unmute() {
         this.context.video.muted = false;
+
+        // Last volume is below muted limit
         const below_muted_max = this.#last_volume <= this.context.config.get( 'sound.mutedmax' );
+
+        // Set the min volume, by default this is 0
         if ( below_muted_max ) this.#last_volume = this.context.config.get( 'sound.volumemin' );
+
+        // If this is the first unmute action and last volume is still in a muted state
         if ( this.#first_unmute && this.#last_volume <= this.context.config.get( 'sound.mutedmax' ) ) {
+
+            // Set an initial unmute volume, this only happens on the first unmute for the given instance
             this.#last_volume = this.context.config.get( 'sound.firstunmute' )
             this.#first_unmute = false;
         }
+
+        // Set the actual volume
         this.context.video.volume = this.#last_volume / 100;
     }
 }
